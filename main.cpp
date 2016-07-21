5fa533a84260415af2954fd072435e89a990aad5
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-21 18:51:30 +0200

    Adds comments describing the process which will be followed on worker nodes

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ int main(int argc, char** argv) {
 		qpid::messaging::Receiver receiver = session.createReceiver(address);
 		qpid::messaging::Message message = receiver.fetch(qpid::messaging::Duration::SECOND * 1); // 1s timeout
 		session.acknowledge();
+		receiver.fetch()
 
 		std::string content = message.getContent();
 
@@ -37,6 +38,24 @@ int main(int argc, char** argv) {
 		boost::shared_ptr<apache::thrift::transport::TMemoryBuffer> tMemoryBuffer(new apache::thrift::transport::TMemoryBuffer(buffer, uint32_t(message.getContentSize())));
 		boost::shared_ptr<apache::thrift::protocol::TBinaryProtocol> protocol(new apache::thrift::protocol::TBinaryProtocol(tMemoryBuffer));
 		com::codinginfinity::benchmark::management::messages::Message message1;
+		message1.read(protocol.get());
+
+
+		// Decompress the algorithm
+		// Decompress the dataset into the root of the extracted algorithm directory
+		// Build the user's application
+		// Fork process and start user's process
+		// In parent process start socket to listen for client for API ready call from client. Can we bind to socket before
+		// we fork, does *nix copy a process bound sockets also in forking? Research needs to be done
+		// Get user's processs ID
+		// Get type of Job
+		// Switch on type of job
+		// Every specified interval, probe for measurement and add to results strcuture, while also monitoring if user
+		// process is still active,
+		// When user process exits, push result strucutre onto queue
+		// Go back to start of loop and check if there is more jobs on the queue
+		// Rinse and repeat
+
 		message1.printTo(std::cout);
 
 		connection.close();
