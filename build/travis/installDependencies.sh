235d714c8be8b7aa2573deb1d12e2f8bc415a27c
Andrew Broekman <andrewbroekman@gmail.com>
2016-07-24 17:15:14 +0200

    Fixes errors in dependency installation script

--- build/travis/installDependencies.sh
+++ build/travis/installDependencies.sh
@@ -11,9 +11,9 @@ sh ${SCRIPTPATH}/installCXXDependencies.sh
 PROTON_TEMP_DIRECTORY="$(mktemp -d)"
 cd ${PROTON_TEMP_DIRECTORY}
 git clone git://git.apache.org/qpid-proton.git
-cd qpid
-mkdir cmake-build
-cd cmake build
+cd qpid-proton
+mkdir cmake_build
+cd cmake_build
 cmake ..
 make all
 make install
@@ -23,8 +23,8 @@ QPID_TEMP_DIRECTORY="$(mktemp -d)"
 cd ${QPID_TEMP_DIRECTORY}
 git clone git://git.apache.org/qpid-cpp.git
 cd qpid-cpp
-mkdir cmake-build
-cd cmake build
+mkdir cmake_build
+cd cmake_build
 make ..
 make all
 make install
@@ -34,8 +34,8 @@ THRIFT_TEMP_DIRECTORY="$(mktemp -d)"
 cd ${THRIFT_TEMP_DIRECTORY}
 git clone git://git.apache.org/thrift.git
 cd thrift
-mkdir cmake-build
-cd cmake build
+mkdir cmake_build
+cd cmake_build
 cmake ..
 make
 make install
@@ -45,8 +45,8 @@ SIGAR_TEMP_DIRECTORY="$(mktemp -d)"
 cd ${SIGAR_TEMP_DIRECTORY}
 git clone https://github.com/hyperic/sigar.git
 cd siger
-mkdir cmake-build
-cd cmake build
+mkdir cmake_build
+cd cmake_build
 cmake ..
 make
 make install
@@ -56,8 +56,8 @@ LIBARCHIVE_TEMP_DIRECTORY="$(mktemp -d)"
 cd ${LIBARCHIVE_TEMP_DIRECTORY}
 git clone https://github.com/libarchive/libarchive.git
 cd libarchive
-mkdir cmake-build
-cd cmake build
+mkdir cmake_build
+cd cmake_build
 cmake ..
 make
 make install
