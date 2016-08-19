723b750b7856aa6cc7a31c621a3a37e8f3c525bd
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-08-19 15:28:01 +0200

    Wall timing implemented using Chrono

--- src/WallClockMeasurementType.cpp
+++ src/WallClockMeasurementType.cpp
@@ -2,6 +2,7 @@
 // Created by reinhardt on 2016/08/17.
 //
 
+#include <chrono>
 #include "WallClockMeasurementType.h"
 
 std::vector<com::codinginfinity::benchmark::management::thrift::messages::Measurement> 
@@ -10,6 +11,43 @@ std::vector<com::codinginfinity::benchmark::management::thrift::messages::Measur
     // Every specified interval, probe for measurement and add to results structure, while also monitoring if user
     // process is still active.
     // When user process exits, push result structure onto queue
+
+    if(std::chrono::high_resolution_clock::is_steady){
+        //if high_resolution_clock is steady use it
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        /*
+         * Program Execution !!
+         */
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto start_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(start);
+        auto end_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(end);
+
+        std::chrono::duration<int, std::milli> durationAsIntergerMilliseconds = end_ms - start_ms;
+
+        int intVal = durationAsIntergerMilliseconds.count();
+
+    }else{
+        //if high_resolution_clock is not steady use steady_clock
+
+        auto start = std::chrono::steady_clock::now();
+
+        /*
+         * Program Execution !!
+         */
+
+        auto end = std::chrono::steady_clock::now();
+
+        auto start_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(start);
+        auto end_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(end);
+
+        std::chrono::duration<int, std::milli> durationAsIntergerMilliseconds = end_ms - start_ms;
+
+        int intVal = durationAsIntergerMilliseconds.count();
+    }
     return NULL;
 }
 
