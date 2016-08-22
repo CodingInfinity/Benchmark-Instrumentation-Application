7db25f36e40ad57c578a2b696b29b82f91269580
Andrew Broekman <andrewbroekman@gmail.com>
2016-08-22 10:24:27 +0200

    Add missing dependency in installCXXDependencies.sh for Travis build

--- build/travis/installCXXDependencies.sh
+++ build/travis/installCXXDependencies.sh
@@ -5,5 +5,5 @@
 sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
 sudo apt-get update -qq
 
-sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make cmake git
+sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make cmake git libuuid1
 dpkg -S /usr/include/boost/version.hpp
