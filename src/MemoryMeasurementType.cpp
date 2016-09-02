5e7599c2886864e07f101a31f4c87a2f4326a7d6
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-09-02 15:05:27 +0200

    Untested implementation of memory measuring

--- src/MemoryMeasurementType.cpp
+++ src/MemoryMeasurementType.cpp
@@ -2,11 +2,13 @@
 // Created by reinhardt on 2016/08/17.
 //
 
+#include <chrono>
+#include <sigar.h>
 #include "MemoryMeasurementType.h"
 
 void MemoryMeasurementType::measure(com::codinginfinity::benchmark::management::thrift::messages::JobSpecificationMessage jobSpecification,
                                     std::string command,
-                                    std::vector<com::codinginfinity::benchmark::management::thrift::messages::Measurement*>*) {
+                                    std::vector<com::codinginfinity::benchmark::management::thrift::messages::Measurement*>* measurements) {
     // Every specified interval, probe for measurement and add to results structure, while also monitoring if user
     // process is still active.
     // When user process exits, push result structure onto queue
@@ -21,12 +23,30 @@ void MemoryMeasurementType::measure(com::codinginfinity::benchmark::management::
     } else if (child_process_id == 0) {
         // If the child_process_id == 0, then we are in the child process
         // Load child process
+        system(command.c_str());
     } else {
-        // If the child_process_id is greater than 0, we are then in the parent process
-        // User's process ID is in child_process_id
+        unsigned long milliseconds_since_epoch =
+                std::chrono::duration_cast<std::chrono::seconds>
+                        (std::chrono::system_clock::now().time_since_epoch()).count();
 
-        // Get type of Job
-        // Switch on type of job
+        int probeInterval = jobSpecification.probeInterval;
+        int timeout = jobSpecification.timeout;
+
+        sigar_t *sigar;
+        sigar_open(&sigar);
+
+        for (int i = 0; i < timeout; i += probeInterval) {
+            com::codinginfinity::benchmark::management::thrift::messages::Measurement *measurement = new com::codinginfinity::benchmark::management::thrift::messages::Measurement();
+            sigar_proc_mem_t mem;
+            sigar_proc_mem_get(sigar, child_process_id, &mem);
+            measurement->value = mem.resident;
+            measurement->timestamp = std::chrono::duration_cast<std::chrono::seconds>
+                    (std::chrono::system_clock::now().time_since_epoch()).count();
+            measurements->push_back(measurement);
+            sleep(probeInterval);
+        }
+        sigar_close(sigar);
+    }
 
     }
 }
